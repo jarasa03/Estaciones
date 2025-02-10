4cf2ab2b7f24faecf4eaf8aef291bd0299c73a3d
jarasa03 <javierarruabarrena03@gmail.com>
2025-02-10 20:27:14 +0100

    feat: :sparkles: Creación de vista FichaEstacion

---
+++ resources/views/estaciones/FichaEstacion.blade.php
@@ -0,0 +1,15 @@
+<!-- resources/views/estaciones/FichaEstacion.blade.php -->
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ficha de la Estación</title>
+    <link href="{{ asset('css/fichaEstacion.css') }}" rel="stylesheet"> <!-- Vincula el archivo CSS -->
+</head>
+<body>
+    <h1>Ficha de la Estación</h1>
+
+    <!-- Aquí pondrás el contenido específico de la estación -->
+</body>
+</html>
